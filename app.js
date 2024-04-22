7436bef4391b098ed6f271a13f0325a7cbd6423d
chevliom11 <chevliom11@gmail.com>
2024-04-23 03:31:21 +0530

    changes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app= express();
+const cors = require('cors')
 const multer = require('multer');
 require('dotenv').config();
 const Customer = require('./routes/Customer.js');
 const UserRoutes = require('./routes/User.js');
 const AdminRoutes = require('./routes/Admin.js');
 
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
